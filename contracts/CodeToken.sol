035e195a61022479dd259f74df01f5334f0c33cf
Timur Borkhodoev <timur.borkhodoev@mail.utoronto.ca>
2018-03-27 16:38:16 -0400

    Add network settings + adding content contract func

--- contracts/CodeToken.sol
+++ contracts/CodeToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.8;
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract CodeToken is MintableToken {
@@ -8,12 +8,25 @@ contract CodeToken is MintableToken {
 	uint public INITIAL_SUPPLY = 0;
 	uint256 public payoutCreator = 1000;
 	uint256 public payoutVoter = 10;
+	uint256 public durationCost = 5;
+	uint256 public currentPromotedContentId = 0;
+  mapping(uint256 => string) promotedContent;
 
 	function CodeToken() public {
 		balances[msg.sender] = 0;
 	}
 
-	function payout(address _creator,address[] _addresses, uint256 len) onlyOwner public returns (bool) {
+	function addPromotedContent(string description, uint256 duration) public returns (bool) {
+		if(transferFrom(msg.sender, 0x0000000000000000000000000000000000000000, duration * durationCost)) {
+			promotedContent[currentPromotedContentId] = description;
+			currentPromotedContentId = currentPromotedContentId.add(1);
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	function payout(address _creator, address[] _addresses, uint256 len) onlyOwner public returns (bool) {
 		uint256 toBeMinted = len * payoutVoter + payoutCreator;
 		totalSupply_ = totalSupply_.add(toBeMinted);
 		balances[_creator] = balances[_creator].add(payoutCreator);
@@ -22,5 +35,4 @@ contract CodeToken is MintableToken {
 		}
 		return true;
 	}
-
 }
--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x5cc82734bb202eb8b9f35f399a5a1b3ebeb3bcdb", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
